6c77cf068ecb3a910159bfd739edd1e45044da6d
matstatman <matstatman@users.noreply.github.com>
2022-05-18 18:59:48 +0200

    Update and rename unifi.sh to meshcommander.sh

---
+++ rc.d/meshcommander.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# REQUIRE: FILESYSTEMS
+# REQUIRE: NETWORKING
+# PROVIDE: meshcommander
+
+. /etc/rc.subr
+
+name="meshcommander"
+rcvar="meshcommander_enable"
+start_cmd="meshcommander_start"
+stop_cmd="meshcommander_stop"
+
+pidfile="/var/run/${name}.pid"
+
+load_rc_config ${name}
+
+meshcommander_start()
+{
+  if checkyesno ${rcvar}; then
+    echo "Starting meshcommander. "
+    cd /usr/local/meshcommander
+    node node_modules/meshcommander --any &
+    echo $! > $pidfile
+
+  fi
+}
+
+meshcommander_stop()
+{
+
+  if [ -f $pidfile ]; then
+    echo -n "Signaling the meshcommander  to stop..."
+
+    kill -9 `cat $pidfile`
+
+    # Remove the pid file:
+    rm $pidfile
+
+    echo " stopped.";
+  else
+    echo "There is no pid file."
+  fi
+}
+
+run_rc_command "$1"
